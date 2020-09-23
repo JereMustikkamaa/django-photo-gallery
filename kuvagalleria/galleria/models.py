6a7de91de251cdd6022d39116f22f9a41e5983ba
JereMustikkamaa <jere_mus@hotmail.com>
2020-09-23 20:48:19 +0300

    Image model created

--- kuvagalleria/galleria/models.py
+++ kuvagalleria/galleria/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Image(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    title = models.CharField(max_length=20)
+    image = models.ImageField()
+    private = models.BooleanField(default=False)
+    rating = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    date = models.DateField(auto_now_add=True)
+    description = models.CharField(max_length=100, blank=True)
+    # size
+    # resolution
+    def __str__(self):
+        return self.title
+    
