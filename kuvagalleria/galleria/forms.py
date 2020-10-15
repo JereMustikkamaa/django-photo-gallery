409666743c40e3c8311e0b6fb9df84c60c1b423f
Jukkendahl <jukka.virkkula90@gmail.com>
2020-10-16 00:11:15 +0300

    minor changes

--- kuvagalleria/galleria/forms.py
+++ kuvagalleria/galleria/forms.py
@@ -3,7 +3,7 @@ from .models import Image
 from django.core.validators import RegexValidator
 
 class ImageForm(forms.ModelForm):
-    subfolder = forms.CharField(required=False, validators=[RegexValidator('^\w+$', message="Subfolder name can only contain aphanumeric characters")])
+    subfolder = forms.CharField(required=False, validators=[RegexValidator('^\w+$', message="Subfolder name can only contain alphanumeric characters")])
     class Meta:
         model = Image
         fields = ['title', 'subfolder', 'image', 'private', 'description' ]
