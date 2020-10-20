434a436104116e1be99968975d24be62d9c0d9aa
JSayos <jsaajos@gmail.com>
2020-10-20 17:52:58 +0300

    Minor bug fix on login

--- kuvagalleria/login/views.py
+++ kuvagalleria/login/views.py
@@ -17,7 +17,7 @@ def home_view(request):
 
 def signin(request):
     if request.method == "POST":
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         user = authenticate(username=username, password=password)
         if user:
